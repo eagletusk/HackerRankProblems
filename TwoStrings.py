071b36199ad8959472f7cf137e25367f715b085c
Paul Subzak <sobczak.paul@gmail.com>
2020-05-02 20:12:26 +0000

    added two Pairs

---
+++ TwoStrings.py
@@ -0,0 +1,12 @@
+def twoStrings(s1, s2):
+    if(len(s1) > len(s2)):
+        for i in s2:
+            if i in s1:
+                return "YES"
+        return "NO"    
+    else:
+        for i in s1:
+            if i in s2:
+                return "YES"
+        return "NO" 
+        
